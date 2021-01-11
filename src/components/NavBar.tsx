a2b3263de50f640ea8f34a9497e5706138ba7561
asce-del <vlad.yaromiy@gmail.com>
2021-01-11 20:31:31 +0200

    cache urql

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import NextLink from "next/link";
+import { Box, Link, Flex, Text, Button } from "@chakra-ui/core";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  // data is loading
+  if (fetching) {
+    body = null;
+
+    // user is not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2} color="white">
+            login
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white">register</Link>
+        </NextLink>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Box>
+        <Flex align="center">
+          <Text color="white" mr={5} fontSize="x">
+            {data.me.username}
+          </Text>
+          <Button variant="link">logout</Button>
+        </Flex>
+      </Box>
+    );
+  }
+
+  return (
+    <Flex bg="black" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
